a020ebd84014f09adb97140914fb91df40d9e4fa
Syed <youremail@example.com>
2024-10-10 12:35:13 +0530

    this is a comment

--- class4.js
+++ class4.js
@@ -1,1 +1,62 @@
-console.log(4);
+// conditional statement in js
+// const age = 22;
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else {
+//   console.log("you can't vote");
+// }
+
+// new one
+// const age = 81;
+// if (age >= 18) {
+//   console.log("you can vote");
+// } else if (age >= 80) {
+//   console.log("you are old");
+// } else {
+//   console.log("you can't vote");
+// }
+
+// new
+
+// ternary operator
+// const age = 22;
+// age >= 18 ? console.log("yess") : console.log("no");
+
+// new
+// const age = 12;
+// let result = age >= 18 ? "yes" : "no";
+// console.log(result);
+
+//new
+// switch case
+// const option = 1;
+// switch (option) {
+//   case 1:
+//     console.log("namaste");
+//     break;
+//   case 2:
+//     console.log("hello");
+//     break;
+//   default:
+//     console.log("please enter correct");
+//     break;
+// }
+
+// new
+const a = 30;
+const opt = "*";
+const b = 40;
+switch (opt) {
+  case "+":
+    console.log(a + b);
+    break;
+  case "-":
+    console.log(a - b);
+    break;
+  case "*":
+    console.log(a * b);
+    break;
+  default:
+    console.log("invalit value");
+    break;
+}
---
+++ class5.js
@@ -0,0 +1,30 @@
+// logical operatord
+// AND && dono condtion true answer true
+// OR || 1 condtion true answer true
+// NOT true ku false false ku true
+
+// const age = 20;
+// const gender = "male";
+// if (age >= 18 && gender == "male") {
+//   console.log("you are adult");
+// } else {
+//   console.log("not adult");
+// }
+
+// new
+// const age = 20;
+// const gender = "male";
+// if (age >= 18 || gender == "males") {
+//   console.log("you are adult");
+// } else {
+//   console.log("not adult");
+// }
+
+// new
+// const number = 5;
+// if (number % 2 != 0) {
+//   console.log("odd");
+// }
+
+// console.log(!true);
+// console.log(!false);
