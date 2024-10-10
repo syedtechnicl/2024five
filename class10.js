5fa5de2310903edbe9646118c93beae6a3d37eed
Syed <youremail@example.com>
2024-10-10 21:10:45 +0530

    this is a new commit

---
+++ class10.js
@@ -0,0 +1,23 @@
+// console.log("class 10");
+// Arrays
+// const students = ["piyush", "Jhon", "Jane", "Bill"];
+// // console.log(students.length);
+// console.log(students[0]);
+// students[0] = "syed";
+// console.log(students[0]);
+// console.log(students[1]);
+
+// students.push("alexa");
+// console.log(students);
+
+// const mark = [1, 2, 3, 4, 5];
+// mark.push(6);
+// const myarray = [1, true, "piyush", "name", mark];
+// console.log(myarray);
+// console.log(myarray.indexOf("piyush"));
+
+const students = ["first", "second", "third"];
+// students.pop();
+// students.pop();
+students.reverse();
+console.log(students);
---
+++ class9.js
@@ -0,0 +1,22 @@
+// console.log("class 9");
+
+// Higher order functions and callback
+
+// function add(a, b, cb) {
+//   let result = a + b;
+//   cb(result);
+// }
+// add(2, 4, function (val) {
+//   console.log(val);
+// });
+
+// add(2, 3, (val) => console.log(val)); Arrow function
+
+// new
+// function add(a, b, cb) {
+//   let result = a + b;
+//   cb(result);
+//   return () => console.log(result);
+// }
+// let resultFunction = add(2, 4, () => {});
+// resultFunction();
